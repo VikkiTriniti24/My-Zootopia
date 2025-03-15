96093c58a970621a9f543bb8a76b5cdb5b0c358a
Viktoria <viktoria.grolik@web.de>
2025-03-15 14:09:54 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -3,18 +3,24 @@ import json
 with open("animals_data.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
-
 output = ''  # Leerer String für die Tierdaten
 
 for animal_data in data:
+    name = animal_data.get("name", "Unknown")
+    diet = animal_data.get("characteristics", {}).get("diet", "Unknown")
+    location = ", ".join(animal_data.get("locations", ["Unknown"]))  # Alle Locations als String
+    type_ = animal_data.get("characteristics", {}).get("type", "Unknown")
+
     output += '<li class="cards__item">\n'
-    output += f"    Name: {animal_data['name']}<br/>\n"
-    output += f"    Diet: {animal_data['characteristics'].get('diet', 'Unknown')}<br/>\n"
-    output += f"    Location: {animal_data.get('locations', ['Unknown'])[0]}<br/>\n"
-    output += f"    Type: {animal_data['characteristics'].get('type', 'Unknown')}<br/>\n"
+    output += f'  <div class="card__title">{name}</div>\n'
+    output += '  <p class="card__text">\n'
+    output += f'      <strong>Diet:</strong> {diet}<br/>\n'
+    output += f'      <strong>Location:</strong> {location}<br/>\n'
+    output += f'      <strong>Type:</strong> {type_}<br/>\n'
+    output += '  </p>\n'
     output += '</li>\n'
 
-print(output)
+print(output)  # Testausgabe
 
 with open("animals_template.html", "r", encoding="utf-8") as file:
     html_template = file.read()
