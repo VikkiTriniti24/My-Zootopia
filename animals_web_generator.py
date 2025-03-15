200c7c514745142ee21cdb77e0be7708595623ce
Viktoria <viktoria.grolik@web.de>
2025-03-15 10:05:21 +0100

    Generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -3,12 +3,18 @@ import json
 with open("animals_data.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
-output = ""
+
+output = ''  # Leerer String für die Tierdaten
+
 for animal_data in data:
-    output += f"Name: {animal_data['name']}\n"
-    output += f"Diet: {animal_data['characteristics'].get('diet', 'Unknown')}\n"
-    output += f"Location: {animal_data.get('locations', ['Unknown'])[0]}\n"
-    output += f"Type: {animal_data['characteristics'].get('type', 'Unknown')}\n\n"
+    output += '<li class="cards__item">\n'
+    output += f"    Name: {animal_data['name']}<br/>\n"
+    output += f"    Diet: {animal_data['characteristics'].get('diet', 'Unknown')}<br/>\n"
+    output += f"    Location: {animal_data.get('locations', ['Unknown'])[0]}<br/>\n"
+    output += f"    Type: {animal_data['characteristics'].get('type', 'Unknown')}<br/>\n"
+    output += '</li>\n'
+
+print(output)
 
 with open("animals_template.html", "r", encoding="utf-8") as file:
     html_template = file.read()
@@ -18,7 +24,5 @@ html_output = html_template.replace("__REPLACE_ANIMALS_INFO__", output)
 with open("animals.html", "w", encoding="utf-8") as file:
     file.write(html_output)
 
-print("Generiertes HTML:\n")
-print(html_output)
-
 print("\n 'animals.html' wurde erfolgreich erstellt! Öffne die Datei im Browser.")
+
